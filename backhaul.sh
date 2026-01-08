5c645920e5189407d19a3cdcd7431077011d4156
hossein-m18 <hosseinm18@gmail.com>
2026-01-08 20:46:43 +0330

    Update backhaul.sh

--- backhaul.sh
+++ backhaul.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
-apt update -y
-apt install wget -y
-wget -O /etc/logo2.sh https://raw.githubusercontent.com/Azumi67/Backhaul_script/refs/heads/main/logo.sh
-chmod +x /etc/logo2.sh
-if [ -f "backhaul.py" ]; then
-    rm backhaul.py
-fi
-wget https://github.com/Azumi67/Backhaul_script/releases/download/script/backhaul.py
-python3 backhaul.py
+## Extremely Managed Server Script
+sudo apt update
+sudo apt install -y python3 python3-pip curl
+
+install_package_if_available() {
+    package_name=$(apt search "$1" 2>/dev/null | grep -o "^$1\S*")
+    if [[ -n "$package_name" ]]; then
+        sudo apt install -y "$package_name"
+    else
+        echo "Package $1 not found in the repositories."
+    fi
+}
+
+install_package_if_available "python3-netifaces"
+install_package_if_available "python3-colorama"
+install_package_if_available "python3-requests"
+sudo apt install -y python3-venv
+python3 -m venv /tmp/my_env
+source /tmp/my_env/bin/activate
+
+pip install --upgrade pip
+pip install netifaces colorama requests
+pip list
+
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/hossein-m18/Backhaul_script/refs/heads/main/backhaul.sh)"
+deactivate
